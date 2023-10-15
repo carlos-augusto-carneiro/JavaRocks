bf6f7acb5a43bdd396674572858fe0b85ade01af
Carlos Augusto <carlosaugustoestudante@hotmail.com>
2023-10-15 00:21:49 -0300

    pronto para deploy

--- src/main/java/br/com/carloscarneiro/todolist/user/UserController.java
+++ src/main/java/br/com/carloscarneiro/todolist/user/UserController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import at.favre.lib.crypto.bcrypt.BCrypt;
-import lombok.experimental.var;
+
 
 
 @RestController
